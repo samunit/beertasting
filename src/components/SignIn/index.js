490d61c4f9e2d305c90da0ad25de7c2a1881edf3
Alexander Welin <tifa@Alexanders-MacBook-Pro.local>
2020-03-30 21:19:47 +0200

    Sign in added

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import { SignUpLink } from '../SignUp';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes'
+
 const SignInPage = () => (
   <div>
     <h1>Sign In</h1>
+    <SignInForm />
+    <SignUpLink />
   </div>
 );
 
 const INITAL_STATE = {
-  username: '',
+  email: '',
   password: '',
   error: null
 }
@@ -16,7 +24,56 @@ class SignInFormBase extends Component {
     super(props);
     this.state = { ...INITAL_STATE };
   }
+    onSubmit = event => {
+      const { email, password } = this.state;
+      this.props.firebase
+        .signInWithEmailAndPassword(email, password)
+        .then(() => {
+          this.setState({...this.INITAL_STATE});
+          this.props.history.push(ROUTES.HOME);
+        })
+        .catch((error) => {
+          this.setState({error});
+        })
+    };
+    onChange = event => {
+      this.setState({ [event.target.name]: event.target.value });
+    };
 
+    render() {
+      const {email, password, error} = this.state;
+      const isInvalid = password === '' || email === '';
+
+      return (
+        <form onSubmit={this.onSubmit}>
+          <input
+            name="email"
+            value={email}
+            onChange={this.onChange}
+            type="text" 
+            placeholder="Email"
+          />
+          <input
+            name="password"
+            value={password}
+            onChange={this.onChange}
+            type="password"
+            placeholder="Password"
+          />
+          <button disabled={isInvalid} type="submit">
+            Sign In
+          </button>
+
+          {error && <p>{error.message}</p>}
+        </form>
+      );
+    };
 }
 
+const SignInForm = compose(
+  withRouter,
+  withFirebase,
+)(SignInFormBase);
+
 export default SignInPage;
+export { SignInForm };
